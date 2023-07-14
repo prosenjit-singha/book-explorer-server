e7b6925749426844b032b48087ffd1428f1c1e1e
PJAheibam <prosenjitsingha28@gmail.com>
2023-07-14 20:39:02 +0600

    handle zod error added

---
+++ src/errors/handlers/handleZodError.ts
@@ -0,0 +1,14 @@
+import { ZodError } from "zod";
+
+const handleZodError = (error: ZodError) => {
+  const err: { [key: string]: string } = {};
+
+  Object.values(error.issues).forEach(issue => {
+    const path = issue.path[issue.path.length - 1];
+    err[path] = issue.message;
+  });
+
+  return err;
+};
+
+export default handleZodError;
