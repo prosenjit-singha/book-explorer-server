c6bacdd5e48324c4dce1367386be7d37bbf16e28
PJAheibam <prosenjitsingha28@gmail.com>
2023-07-14 20:32:42 +0600

    mongodb packages installed

---
+++ src/types/apiResponse.type.ts
@@ -0,0 +1,17 @@
+import { SortOrder } from "mongoose";
+
+type ApiResponse<T = unknown> = {
+  status: number;
+  error: null | { [key: string]: string } | string;
+  message: string;
+  data: T | null;
+  meta?: {
+    page: number;
+    limit: number;
+    totalResults: number;
+    sortBy: string;
+    sortOrder: SortOrder;
+  };
+};
+
+export default ApiResponse;
